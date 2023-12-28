7452b5fc572dc509fdb7b8bb0fbce6c98a668a04
cho-hyeonjin <gusls2005@gmail.com>
2023-12-28 22:56:25 +0900

    comment: Routing Ver.2 - router.addRoute > fragment 파싱 시 사용되는 paramName 흐름

--- Chapter06/00.2/router.js
+++ Chapter06/00.2/router.js
@@ -1,11 +1,9 @@
 // ! Router 구성 (Contoller Configuration)
 // ! > Ver.2.1 (프로그래밍 방식 ) --- extractUrlParams 메서드 && 정규식을 이용한
 
-const ROUTE_PARAMETER_REGEXP = /:(\w+)/g; // ":"로 시작하는 문자열을 찾아서 ":"을 제외한 문자열을 나타냄. (라우팅에서 동적으로 변하는 Route Parameter)
-const URL_FRAGMENT_REGEXP = "([^\\/]+)"; // "/"를 제외한 모든 문자열
+const ROUTE_PARAMETER_REGEXP = /:(\w+)/g;
+const URL_FRAGMENT_REGEXP = "([^\\/]+)";
 
-// todo: extractUrlParams 메서드는 왜 필요할까?
-// → winodw.location.hash값과 일치하는 url에 매핑된 컴포넌트를 찾기 위해
 const extractUrlParams = (route, windowHash) => {
   const params = {};
 
@@ -15,7 +13,7 @@ const extractUrlParams = (route, windowHash) => {
 
   const matches = windowHash.match(route.testRegExp);
 
-  matches.shift();
+  matches.shift(); // shift는 왜 하는거?
 
   matches.forEach((paramValue, index) => {
     const paramName = route.params[index]; // ? paramName은 route객체의 params[index] 다. → extractUrlParams 호출부 보자.
@@ -25,7 +23,6 @@ const extractUrlParams = (route, windowHash) => {
   return params;
 };
 
-// todo: 이 무명 함수는 index.js(Controlling 부)에서 createRouter라는 이름으로 import되어 사용될 예정!
 export default () => {
   const routes = [];
   let notFound = () => {};
@@ -36,21 +33,18 @@ export default () => {
   const checkRoutes = () => {
     const { hash } = window.location;
 
-    // 일치하는 경로 찾는 로직
     const currentRoute = routes.find((route) => {
       // ? currentRoute는 Router 배열(routes)에서 textRegExp정규식을 만족하는 첫번째 요소를 리턴
       const { testRegExp } = route;
       return testRegExp.test(hash);
     });
-    // 분기처리
-    // 1(N). 先 예외 처리 - 일치하는 경로 발견 X → notFound메서드 실행 & 리턴
+
     if (!currentRoute) {
       notFound();
       return;
     }
 
     const urlParams = extractUrlParams(currentRoute, window.location.hash); // ? route객체는 currentRoute 다. → currentRoute 보자.
-    // 2(Y). 일치하는 경로 발견 O → 라우터 컨테이너의 component 교체
     currentRoute.component(urlParams);
   };
 
@@ -58,15 +52,13 @@ export default () => {
   router.addRoute = (fragment, component) => {
     const params = [];
 
-    // index.js > router.addRoute(fragment, 컴포넌트)의 fragment를 정규식으로 파싱
     const parsedFragment = fragment
-      // fragment에서 ROUTE_PARAMETER_REGEXP 정규식과 일치하는 문자열 🔄 콜백함수가 리턴한 URL_FRAGMENT_REGEXP 정규식(동적인 값)
       .replace(ROUTE_PARAMETER_REGEXP, (match, paramName) => {
         // ? paramName은 어디서 전달받는 값? → extractUrlParams 메서드 봐라
         params.push(paramName);
         return URL_FRAGMENT_REGEXP;
       })
-      .replace(/\//g, "\\/"); // 그 문자열에서 "/\//g"과 일치하는 문자열 🔄 "\\/"로 변경
+      .replace(/\//g, "\\/");
 
     console.log(`^${parsedFragment}$`);
 
@@ -75,12 +67,6 @@ export default () => {
       component,
       params,
     });
-    // Router 배열의 객체 데이터 모양
-    // → {
-    //     testRegExp: 정규식,
-    //     component: pages.컴포넌트명에 매핑된 값(현재 기준 문자열)
-    //     params: [ paramName들, ... ]
-    //   }
 
     return router;
   };
